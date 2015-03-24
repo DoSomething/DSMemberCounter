8cb39b0ddc81545c3866a4fc5e497a5841674002
Joe Kent <thedeadlybutter@gmail.com>
2015-03-24 14:41:56 -0400

    Made URL's config based for multi enviornments, added update loop

--- app.js
+++ app.js
@@ -29,12 +29,13 @@ function getMemberCount(callback) {
 function updateApplications() {
   getMemberCount(function(data) {
     updateDrupal(data);
+    //TODO: Update lobby dash
   });
 }
 
 function updateDrupal(data) {
   request
-   ..post("https://www.dosomething.org/api/v1/auth/login")
+   .post(config.DRUPAL_AUTH)
    .set('Content-Type', 'application/json')
    .set('Accept', 'application/json')
    .send({"username": config.DRUPAL_USERNAME, "password": config.DRUPAL_PASSWORD})
@@ -44,14 +45,14 @@ function updateDrupal(data) {
       var drupalSessid = raw.sessid;
       var drupalSessionName = raw.session_name;
       request
-       .post('https://www.dosomething.org:8888/api/v1/system/set_variable')
+       .post(config.SET_VAR)
        .set('Accept', 'application/json')
        .set("Content-type", "application/json")
        .set("X-CSRF-Token", drupalToken)
        .set("Cookie", drupalSessionName + "=" + drupalSessid)
        .send({"name": "dosomething_user_member_count", "value": data.total})
        .end(function(res){
-         console.log("Updated Drupal");
+         console.log("Updated Drupal, Status Code: " + res.status);
       });
   });
 }
@@ -59,4 +60,5 @@ function updateDrupal(data) {
 var server = app.listen(4123, function() {
     console.log('Listening on port %d', server.address().port);
     updateApplications();
+    setTimeout(updateApplications, (5 * 60) * 1000); //Updates every 5 minutes
 });
